aafedb15adaff467c0381e6ea14f449c642ef94c
Daniel Kreider <rxgmt61of@mozmail.com>
2023-10-26 11:14:42 -0300

    Fix exports

--- projects/ngx-search-filter/src/public-api.ts
+++ projects/ngx-search-filter/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ngx-search-filter
  */
 
-export * from './lib/ngx-search-filter.service';
-export * from './lib/ngx-search-filter.component';
+export * from './lib/ngx-filter.pipe';
 export * from './lib/ngx-search-filter.module';
